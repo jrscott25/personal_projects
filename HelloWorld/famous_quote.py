9ad2b533c941a84af3741542a1a6cdeebde7c666
jrscott25 <41653495+jrscott25@users.noreply.github.com>
2024-06-17 21:01:32 -0400

    Changed message to a variable

---
+++ HelloWorld/famous_quote.py
@@ -0,0 +1,5 @@
+quote = "Men are haunted by the vastness of eternity. And so we ask ourselves: will our actions echo across the centuries? Will strangers hear our names long after we are gone, and wonder who we were, how bravely we fought, how fiercely we loved?"
+
+author = "wolfgang peterson"
+
+print(f'{author.title()} once said, "{quote}"')
